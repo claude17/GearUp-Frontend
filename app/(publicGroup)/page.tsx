5f98e4dd76c62408a2c67161dbb4e417ad3e6bdb
claude17 <claude.mta@gmail.com>
2026-08-03 15:49:12 +0600

    initial setup

---
+++ app/(publicGroup)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function HomePage () {
+  return (
+    
+    <div className="flex min-h-svh items-center justify-center">
+      <div>Home page - root route</div>
+      <Button>Click me</Button>
+    </div>
+  )
+}
---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={cn("h-full antialiased", "font-sans", inter.variable)}
+    >
+      <body className="min-h-full flex flex-col">
+
+        {/* <Toaster position="top-right" richColors /> */}
+        {/* {Navbar} */}
+        {/* <TooltipProvider> */}
+          {children}
+        {/* </TooltipProvider> */}
+
+
+        {/* {Footer} */}
+      </body>
+    </html>
+  );
+}

84d7c4600d2c66d0789c747d2d6505fc6a76e8e8
claude17 <claude.mta@gmail.com>
2026-08-17 00:24:34 +0600

    fix: home page

--- app/(publicGroup)/page.tsx
+++ app/(publicGroup)/page.tsx
@@ -1,11 +1,38 @@
-import { Button } from "@/components/ui/button";
+// import FeaturedGear from "./_components/FeaturedGear";
 
-export default function HomePage () {
-  return (
-    
-    <div className="flex min-h-svh items-center justify-center">
-      <div>Home page - root route</div>
-      <Button>Click me</Button>
-    </div>
-  )
+// export default function HomePage() {
+//     return (
+//         <main>
+//             <section className="flex h-[70vh] items-center justify-center border-b">
+//                 <div className="space-y-6 text-center">
+//                     <h1 className="text-6xl font-bold">
+//                         Rent Sports & Outdoor Gear Instantly
+//                     </h1>
+
+//                     <p className="mx-auto max-w-2xl text-muted-foreground">
+//                         Discover premium sports and outdoor equipment from trusted providers.
+//                     </p>
+//                 </div>
+//             </section>
+
+//             <FeaturedGear />
+//         </main>
+//     );
+// }
+
+
+
+
+
+
+
+import { getMe } from '@/service/getMe';
+import { FeaturedGear } from './_components/homepage/FeaturedGear';
+import { HeroSection } from './_components/homepage/HeroSection';
+import { HowItWorks } from './_components/homepage/HowItWorks';
+import { SiteFooter } from '@/components/shared/footer';
+
+export default function HomePage() {
+ 
+  return <main><HeroSection /><FeaturedGear /><HowItWorks /><SiteFooter/></main>
 }

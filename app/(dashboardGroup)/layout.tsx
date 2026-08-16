43a12a95ceeb78a5735a5a3deede40fbf775d5d6
claude17 <claude.mta@gmail.com>
2026-08-17 00:02:38 +0600

    add: dashboard sidebar

--- app/(dashboardGroup)/layout.tsx
+++ app/(dashboardGroup)/layout.tsx
@@ -1,7 +1,11 @@
 // import { Navbar } from '@/components/shared/navbar';
 // import { SidebarProvider } from '@/components/ui/sidebar';
 // import { getMe } from '@/service/getMe';
+import { Navbar } from '@/components/shared/navbar';
+import { SidebarProvider } from '@/components/ui/sidebar';
+import { getMe } from '@/service/getMe';
 import React from 'react';
+import DashboardSidebar from './_components/DashboardSIdebar';
 // import DashboardSidebar from './_components/DashboardSidebar';
 
 const DashboardLayout = async (
@@ -11,14 +15,14 @@ const DashboardLayout = async (
         children: React.ReactNode;
     }
 ) => {
-    // const user = await getMe()
+    const user = await getMe()
     return (
         <div className='min-h-screen flex flex-col'>
             {/* <Navbar user={user} /> */}
-            {/* <SidebarProvider className='flex-1'> */}
-                {/* <DashboardSidebar user={user} /> */}
+            <SidebarProvider className='flex-1'>
+                <DashboardSidebar user={user} />
                 <main className='flex-1 min-w-0'>{children}</main>
-            {/* </SidebarProvider> */}
+            </SidebarProvider>
             
         </div>
     );
